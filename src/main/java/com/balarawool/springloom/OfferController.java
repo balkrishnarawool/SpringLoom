5f5d109f2e8e309ba489292f1d24a221389c51a5
Balkrishna Rawool <balkrishna.rawool@gmail.com>
2023-11-04 22:59:11 +0100

    CF logic added

--- src/main/java/com/balarawool/springloom/OfferController.java
+++ src/main/java/com/balarawool/springloom/OfferController.java
@@ -1,10 +1,12 @@
 package com.balarawool.springloom;
 
 import com.balarawool.springloom.domain.CustomerUtil;
+import com.balarawool.springloom.domain.CustomerUtil.CustomerDetails;
 import com.balarawool.springloom.domain.CustomerUtil.Offer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.StructuredTaskScope;
 
@@ -13,23 +15,24 @@ public class OfferController {
 
     @GetMapping("/offer")
     public Offer getOffer() {
-        var customer = CustomerUtil.getCurrentCustomer();
+        var future1 = CompletableFuture.supplyAsync(CustomerUtil::getCurrentCustomer);
+        var future2 = future1.thenApplyAsync(CustomerUtil::getSavingsData);
+        var future3 = future1.thenApplyAsync(CustomerUtil::getLoansData);
 
-        try (var scope = new StructuredTaskScope.ShutdownOnFailure()) {
-            var task1 = scope.fork(() -> CustomerUtil.getSavingsData(customer));
-            var task2 = scope.fork(() -> CustomerUtil.getLoansData(customer));
+        var customer = future1
+                .exceptionally(th -> {
+                    throw new RuntimeException(th);
+                })
+                .join();
+        var future = future2
+                .thenCombine(future3, ((savings, loans) -> new CustomerDetails(customer, savings, loans)))
+                .thenApplyAsync(CustomerUtil::calculateOffer)
+                .exceptionally(th -> {
+                    throw new RuntimeException(th);
+                });
 
-            scope.join().throwIfFailed();
-
-            var savings = task1.get();
-            var loans = task2.get();
-            var details = new CustomerUtil.CustomerDetails(customer, savings, loans);
-
-            var offer = CustomerUtil.calculateOffer(details);
-            return offer;
-        } catch (InterruptedException | ExecutionException e) {
-            throw new RuntimeException(e);
-        }
+        var offer = future.join();
+        return offer;
     }
 
     @GetMapping("/offer2")
@@ -43,7 +46,7 @@ public class OfferController {
 
             var savings = task1.get();
             var loans = task2.get();
-            var details = new CustomerUtil.CustomerDetails(customer, savings, loans);
+            var details = new CustomerDetails(customer, savings, loans);
 
             var offer = CustomerUtil.calculateOffer(details);
             return offer;
